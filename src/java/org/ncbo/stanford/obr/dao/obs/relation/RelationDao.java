573e26f6d4c5068ce9a6dbbccfecd89b37441845
jpatel <jpatel@ee390ac1-5155-0410-a410-960b64f5fe6a>
2011-06-14 09:57:27 +0000

    Changed query for remove ontology step.

--- src/java/org/ncbo/stanford/obr/dao/obs/relation/RelationDao.java
+++ src/java/org/ncbo/stanford/obr/dao/obs/relation/RelationDao.java
@@ -141,12 +141,27 @@ public class RelationDao extends AbstractObsDao{
 		StringBuffer queryb = new StringBuffer();
 		queryb.append("DELETE ISAPT FROM ");
 		queryb.append(this.getTableSQLName());		
+		queryb.append(" ISAPT ");
+		queryb.append(" WHERE ISAPT.concept_id in (");
+		queryb.append(" SELECT id from  ");
+		queryb.append(ConceptDao.name( ));	
+		queryb.append(" CT ");
+		queryb.append(" WHERE CT.ontology_id in (");
+		queryb.append(" SELECT id from  ");
+		queryb.append(OntologyDao.name());
+		queryb.append(" OT ");
+		queryb.append(" WHERE OT.local_ontology_id in ");
+		queryb.append(" ( ?  ");
+		queryb.append(")));");
+		
+		/*queryb.append("DELETE ISAPT FROM ");
+		queryb.append(this.getTableSQLName());		
 		queryb.append(" ISAPT, ");
 		queryb.append(ConceptDao.name( ));	
 		queryb.append(" CT, ");
 		queryb.append(OntologyDao.name());
 		queryb.append(" OT ");
-		queryb.append(" WHERE ISAPT.concept_id = CT.id AND CT.ontology_id = OT.id AND OT.local_ontology_id = ?");
+		queryb.append(" WHERE ISAPT.concept_id = CT.id AND CT.ontology_id = OT.id AND OT.local_ontology_id = ?");*/
 	
 		deleteEntriesFromOntologyStatement = this.prepareSQLStatement(queryb.toString());
 	}
