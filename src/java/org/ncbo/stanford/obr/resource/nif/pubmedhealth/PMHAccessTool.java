c739cbfb7ad5e4063d4a0c353633590ea41d8878
skharat <sachinkharat@users.noreply.github.com>
2012-10-16 05:24:58 +0000

    modified RAT implementation

--- src/java/org/ncbo/stanford/obr/resource/nif/pubmedhealth/PMHAccessTool.java
+++ src/java/org/ncbo/stanford/obr/resource/nif/pubmedhealth/PMHAccessTool.java
@@ -11,6 +11,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 import obs.obr.populate.Element;
 import obs.obr.populate.Structure;
 import org.jsoup.Jsoup;
@@ -20,31 +21,40 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 /**
- * AccessTool for NIF PubMedHealth
+ * AccessTool for NIF PubMedHealth Drugs
  * @author s.kharat
  */
-public class PMHAccessTool extends AbstractNifResourceAccessTool{
-    
+public class PMHAccessTool extends AbstractNifResourceAccessTool {
+
     private static final String PMH_URL = "http://www.ncbi.nlm.nih.gov/pubmedhealth";
-    private static final String PMH_NAME = "PubMedHealth (via NIF)";
+    private static final String PMH_NAME = "PubMedHealth Drugs (via NIF)";
     private static final String PMH_RESOURCEID = "PMH";
     private static final String PMH_DESCRIPTION = "PubMed Health is a consumer health Web site produced by the National Center for Biotechnology Information (NCBI), a division of the National Library of Medicine (NLM) at the National Institutes of Health (NIH). PubMed Health provides up-to-date information on diseases, conditions, injuries, drugs, supplements, treatment options, and healthy living, with a special focus on comparative effectiveness research from institutions around the world.";
     private static final String PMH_LOGO = "http://neurolex.org/w/images/8/8c/PubMed_Health.PNG";
-    private static final String PMH_ELT_URL = "http://www.ncbi.nlm.nih.gov/pubmedhealth/";    
-    private static final String[] PMH_ITEMKEYS = {"Name", "Title", "Text"};
-    private static final Double[] PMH_WEIGHTS = {1.0, 0.7, 0.9};
-    private static final String[] PMH_ONTOIDS = {Structure.FOR_CONCEPT_RECOGNITION, Structure.FOR_CONCEPT_RECOGNITION, Structure.FOR_CONCEPT_RECOGNITION};
+    private static final String PMH_ELT_URL = "http://www.ncbi.nlm.nih.gov/pubmedhealth/";
+    private static final String[] PMH_ITEMKEYS = {"Name", "How_to_use", "Side_effects", "Other_information", "Why_is_this_medication_prescribed", "Storage_conditions", "In_case_of_emergency", "If_I_forgot_a_dose", "Special_dietary_instructions", "Other_uses"};
+    private static final Double[] PMH_WEIGHTS = {1.0, 0.9, 0.9, 0.8, 0.8, 0.7, 0.9, 0.8, 0.7, 0.7};
+    private static final String[] PMH_ONTOIDS = {Structure.FOR_CONCEPT_RECOGNITION, Structure.FOR_CONCEPT_RECOGNITION, Structure.FOR_CONCEPT_RECOGNITION, Structure.FOR_CONCEPT_RECOGNITION,
+        Structure.FOR_CONCEPT_RECOGNITION, Structure.FOR_CONCEPT_RECOGNITION, Structure.FOR_CONCEPT_RECOGNITION, Structure.FOR_CONCEPT_RECOGNITION,
+        Structure.FOR_CONCEPT_RECOGNITION, Structure.FOR_CONCEPT_RECOGNITION};
     private static Structure PMH_STRUCTURE = new Structure(PMH_ITEMKEYS, PMH_RESOURCEID, PMH_WEIGHTS, PMH_ONTOIDS);
     private static String PMH_MAIN_ITEMKEY = "Name";
     
-    // Constant 
+    // Constants 
     private static final String PMH_Database = "PubMedHealth";
     private static final String PMH_Indexable = "Drug";
-    
+    private static final String pmh_title = "Title";
+    private static final String pmh_text = "Text";
     private static final String PMH_Name = "Name";
-    private static final String PMH_Title = "Title";
-    private static final String PMH_Text = "Text";
-           
+    private static final String How_to_use = "How should this medicine be used?";
+    private static final String Side_effects = "What side effects can this medication cause?";
+    private static final String Other_information = "What other information should I know?";
+    private static final String Why_is_this_medication_prescribed = "Why is this medication prescribed?";
+    private static final String Storage_conditions = "What storage conditions are needed for this medicine?";
+    private static final String In_case_of_emergency = "In case of emergency/overdose";
+    private static final String If_I_forgot_a_dose = "What should I do if I forget a dose?";
+    private static final String Special_dietary_instructions = "What special dietary instructions should I follow?";
+    private static final String Other_uses = "Other uses for this medicine";
     private Map<String, String> localOntologyIDMap;
 
     // constructors
@@ -81,8 +91,7 @@ public class PMHAccessTool extends AbstractNifResourceAccessTool{
 
     @Override
     public String elementURLString(String elementLocalID) {
-        String[] elementId = elementLocalID.split(SLASH_STRING);        
-        return PMH_ELT_URL + elementId[0] ;
+        return PMH_ELT_URL + elementLocalID;
     }
 
     @Override
@@ -145,7 +154,7 @@ public class PMHAccessTool extends AbstractNifResourceAccessTool{
      * get all Elements.
      */
     public HashSet<Element> getAllElements() {
-        logger.info("* Get All Elements for PubMedHealth ... ");
+        logger.info("* Get All Elements for PubMedHealth Drugs... ");
         HashSet<Element> elementSet = new HashSet<Element>();
         int nbAdded = 0;
         int offset = 0;
@@ -159,18 +168,20 @@ public class PMHAccessTool extends AbstractNifResourceAccessTool{
             //parsing data
             do {
                 Document dom = queryFederation(PMH_Database, PMH_Indexable, query, offset, rowCount);
-                if(dom != null){
+                if (dom != null) {
                     Node tableData = dom.getFirstChild();
                     //get total records
                     totalCount = Integer.parseInt(tableData.getAttributes().getNamedItem(resultCount).getNodeValue());
                     offset += rowCount;
-                    
+
                     Node results = tableData.getFirstChild();
 
                     // Iterate over the returned structure 
                     NodeList rows = results.getChildNodes();
                     for (int i = 0; i < rows.getLength(); i++) {
                         String localElementId = EMPTY_STRING;
+                        String attTitle = EMPTY_STRING;
+                        String attValue = EMPTY_STRING;
                         Map<String, String> elementAttributes = new HashMap<String, String>();
 
                         Node row = rows.item(i);
@@ -178,33 +189,63 @@ public class PMHAccessTool extends AbstractNifResourceAccessTool{
                             NodeList vals = row.getChildNodes().item(j).getChildNodes();
                             String name = null;
                             String value = null;
+                            
                             for (int k = 0; k < vals.getLength(); k++) {
                                 if (nodeName.equals(vals.item(k).getNodeName())) {
                                     name = vals.item(k).getTextContent();
                                 } else if (nodeValue.equals(vals.item(k).getNodeName())) {
-                                    value = vals.item(k).getTextContent();
+                                     value = vals.item(k).getTextContent();
                                 }
                             }
-                            if (name.equalsIgnoreCase(PMH_Name)) {                       //Name & localElementId
-                                localElementId =value.substring(value.indexOf(PMH_ELT_URL) + PMH_ELT_URL.length(), value.indexOf(endTag)-1 ) ;      
-                                elementAttributes.put(Structure.generateContextName(PMH_RESOURCEID, PMH_ITEMKEYS[0]),  Jsoup.parse(value).text());                                                
-                            } else if (name.equalsIgnoreCase(PMH_Title)) {               //Text                                                          
-                                elementAttributes.put(Structure.generateContextName(PMH_RESOURCEID, PMH_ITEMKEYS[1]), value);
-                            } else if (name.equalsIgnoreCase(PMH_Text)) {                //Title
-                                elementAttributes.put(Structure.generateContextName(PMH_RESOURCEID, PMH_ITEMKEYS[2]), value);
-                            } 
+                            if (name.equalsIgnoreCase(PMH_Name)) {
+                                localElementId = value.substring(value.indexOf(PMH_ELT_URL) + PMH_ELT_URL.length(), value.indexOf(endTag) - 1);
+                                elementAttributes.put(Structure.generateContextName(PMH_RESOURCEID, PMH_ITEMKEYS[0]), Jsoup.parse(value).text());
+                            } else if (name.equalsIgnoreCase(pmh_text)) {
+                                attValue = value;
+                            } else if (name.equalsIgnoreCase(pmh_title)) {
+                                attTitle = value;
+                            }
+                        }
+                        
+                       
+                        if (attTitle.equalsIgnoreCase(How_to_use) || attTitle.contains("How to Use") 
+                                ) {//|| attTitle.contains("About your treatment") || attTitle.contains("Administering your medication")
+                            elementAttributes.put(Structure.generateContextName(PMH_RESOURCEID, PMH_ITEMKEYS[1]), attValue);
+                        } else if (attTitle.equalsIgnoreCase(Side_effects) || attTitle.contains("risks")) {
+                            elementAttributes.put(Structure.generateContextName(PMH_RESOURCEID, PMH_ITEMKEYS[2]), attValue);
+                        } else if (attTitle.equalsIgnoreCase(Other_information)) {
+                            elementAttributes.put(Structure.generateContextName(PMH_RESOURCEID, PMH_ITEMKEYS[3]), attValue);
+                        } else if (attTitle.equalsIgnoreCase(Why_is_this_medication_prescribed) ) {//|| attTitle.contains("What is")
+                            elementAttributes.put(Structure.generateContextName(PMH_RESOURCEID, PMH_ITEMKEYS[4]), attValue);
+                        } else if (attTitle.equalsIgnoreCase(Storage_conditions)) {
+                            elementAttributes.put(Structure.generateContextName(PMH_RESOURCEID, PMH_ITEMKEYS[5]), attValue);
+                        } else if (attTitle.equalsIgnoreCase(In_case_of_emergency) || attTitle.equalsIgnoreCase("severe reaction")) {
+                            elementAttributes.put(Structure.generateContextName(PMH_RESOURCEID, PMH_ITEMKEYS[6]), attValue);
+                        } else if (attTitle.equalsIgnoreCase(If_I_forgot_a_dose)) {
+                            elementAttributes.put(Structure.generateContextName(PMH_RESOURCEID, PMH_ITEMKEYS[7]), attValue);
+                        } else if (attTitle.equalsIgnoreCase(Special_dietary_instructions)) {
+                            elementAttributes.put(Structure.generateContextName(PMH_RESOURCEID, PMH_ITEMKEYS[8]), attValue);
+                        } else if (attTitle.equalsIgnoreCase(Other_uses)) {                //Title
+                            elementAttributes.put(Structure.generateContextName(PMH_RESOURCEID, PMH_ITEMKEYS[9]), attValue);
                         }
 
                         //Check if elementId is present locally.
                         if (allElementsInET.contains(localElementId)) {
                             continue;
                         } else {
-                            //(SLASH_STRING and i value appended for unique local element Id purpose)
-                            allRowsData.put(localElementId + SLASH_STRING + i, elementAttributes); 
+                            if (allRowsData.containsKey(localElementId)) {
+                                Map<String, String> prevAttributes = allRowsData.get(localElementId);
+                                for (String attribute : elementAttributes.keySet()) {
+                                    prevAttributes.put(attribute, elementAttributes.get(attribute));
+                                }
+                                allRowsData.put(localElementId, prevAttributes);
+                            } else {
+                                allRowsData.put(localElementId, elementAttributes);
+                            }
                         }
                     }
-                }else{
-                    offset += rowCount;                   
+                } else {
+                    offset += rowCount;
                 }
             } while (offset < totalCount);
 
@@ -238,7 +279,7 @@ public class PMHAccessTool extends AbstractNifResourceAccessTool{
                     }
                 }
                 // put the element structure in a new element
-                try {                    
+                try {
                     Element exp = new Element(localElementID, elementStructure);
                     elementSet.add(exp);
                 } catch (Element.BadElementStructureException e) {
@@ -254,5 +295,3 @@ public class PMHAccessTool extends AbstractNifResourceAccessTool{
         return elementSet;
     }
 }
-
-
