25a066935e91cc9bed1837ed39d0bb6d55deeca1
rmalviya <r.malviya@optrasystems.com>
2015-04-03 11:24:31 +0530

    Update all.sh

--- all.sh
+++ all.sh
@@ -6,15 +6,10 @@
 ant clean all
 cd dist
 chmod 777 run.sh
-<<<<<<< HEAD
+
 chmod 777 files/mgrep/mgrep3.0/mgrep
 
 #./run.sh >resource_index_workflow_all.log &
 #tail -f resource_index_workflow_all.log
 ./run.sh &
-=======
 
-#./run.sh >resource_index_workflow_all.log &
-#tail -f resource_index_workflow_all.log
-./run.sh
->>>>>>> origin/branch1.0
