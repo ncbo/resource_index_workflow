38d47de53f9dc21ea9687a8c1b8e1c16c5a1a05d
rmalviya <r.malviya@optrasystems.com>
2015-04-06 11:00:29 +0530

    Update TermDao.java

--- src/java/org/ncbo/stanford/obr/dao/obs/term/TermDao.java
+++ src/java/org/ncbo/stanford/obr/dao/obs/term/TermDao.java
@@ -6,10 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashSet;
 import java.util.Iterator;
-<<<<<<< HEAD
 import java.util.Set;
-=======
->>>>>>> origin/branch1.0
 
 import obs.common.utils.Utilities;
 
